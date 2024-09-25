bd6ecd230a1cd30ff090ad7f3d1bf5019cb2e922
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2024-09-25 23:59:15 +0300

    point light replaces the directional

--- src/shaders/water/fragment.glsl
+++ src/shaders/water/fragment.glsl
@@ -7,7 +7,7 @@ varying float vElevation;
 varying vec3 vNormal;
 varying vec3 vPosition;
 
-#include ../includes/directionalLight.glsl
+#include ../includes/pointLight.glsl
 
 
 void main()
@@ -23,13 +23,15 @@ void main()
      // Light
     vec3 light = vec3(0.0);
 
-    light += directionalLight(
+    light += pointLight(
         vec3(1.0),            // Light color
-        1.0,                  // Light intensity,
+        10.0,                  // Light intensity,
         normal,               // Normal
-        vec3(-1.0, 0.5, 0.0), // Light position
+        vec3(0.0, 0.25, 0.0), // Light position
         viewDirection,        // View direction
-        30.0                  // Specular power
+        30.0,                // Specular power
+        vPosition,          //Position
+        0.95                //Decay
     );
 
     color *= light;
